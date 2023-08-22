cede976315cbf29af310def30a54b409047b5e42
jp-x-g <jpxg-dev@protonmail.com>
2023-08-22 15:03:10 -0700

    Add a route that just fetches from the Signpost URL.

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,25 @@
 var express = require('express');
 var router = express.Router();
+var axios = require('axios');
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/:path(*)', async function(req, res, next) {
+  const path = req.params.path;
+
+  try {
+    const response = await axios.get(`https://en.wikipedia.org/w/api.php`, {
+      params: {
+        action: 'parse',
+        page: `Wikipedia:Wikipedia_Signpost/${path}`,
+        format: 'json'
+      }
+    });
+
+    const text = response.data.parse.text['*'];
+
+    res.send(text);
+  } catch (error) {
+    next(error); // Pass the error to the error handling middleware
+  }
 });
 
 module.exports = router;
